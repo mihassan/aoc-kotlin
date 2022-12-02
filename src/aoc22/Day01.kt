8ef25a07fe2f600b3fc519d306ecf10144e1f34d
Md Imrul Hassan <mihassan@gmail.com>
2022-12-02 18:35:53 +1100

    Solve 2022, Day 01 problem.
    Also, commit missing data files.

---
+++ src/aoc22/Day01.kt
@@ -0,0 +1,19 @@
+package aoc22
+
+import lib.Solution
+
+private val solution = object : Solution<List<List<Int>>, Int>(2022, "Day01") {
+  override fun parse(input: String): List<List<Int>> =
+    input.split("\n\n").map {
+      it.trim().split("\n").map(String::toInt)
+    }
+
+  override fun format(output: Int): String = output.toString()
+
+  override fun part1(input: List<List<Int>>): Int = input.maxOf { it.sum() }
+
+  override fun part2(input: List<List<Int>>): Int =
+    input.map { it.sum() }.sorted().takeLast(3).sum()
+}
+
+fun main() = solution.run()
