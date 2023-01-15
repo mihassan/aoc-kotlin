b5d94b6dba1df78a739b69150afb91d64ee10e7c
Md Imrul Hassan <mihassan@gmail.com>
2023-01-15 16:22:50 +1100

    Update solution for 2022, Day 11 problem by making Game.monkeys as a readonly List, passing onInspectionHandler and throwItemTo functions directly instead of storing in the instances, calculating Game.modulus from monkeys.test.modulus.

--- src/aoc22/Day11.kt
+++ src/aoc22/Day11.kt
@@ -6,43 +6,24 @@ import lib.Solution
 
 private class Game private constructor(
   val modulus: Long,
-  val monkeys: MutableList<Monkey> = mutableListOf(),
+  val monkeys: List<Monkey>,
 ) {
-  private lateinit var onInspectionHandler: ((MonkeyId, Item) -> Item)
+  fun clone() = Game(modulus, monkeys.map { monkey -> monkey.clone() })
 
-  fun runMultipleRounds(rounds: Int) = repeat(rounds) { runSingleRound() }
+  fun runMultipleRounds(rounds: Int, onInspectionHandler: (MonkeyId, Item) -> Item) =
+    repeat(rounds) { runSingleRound(onInspectionHandler) }
 
-  fun onInspection(onInspectionHandler: (MonkeyId, Item) -> Item) {
-    this.onInspectionHandler = onInspectionHandler
-  }
-
-  fun clone(): Game {
-    val game = Game(modulus)
-    monkeys.forEach { monkey ->
-      game.monkeys += monkey.clone(throwItemToFn = game::throwItemTo)
-    }
-    return game
-  }
-
-  private fun runSingleRound() =
-    monkeys.forEach { monkey -> monkey.processItems(onInspectionHandler) }
+  private fun runSingleRound(onInspectionHandler: (MonkeyId, Item) -> Item) =
+    monkeys.forEach { monkey -> monkey.processItems(onInspectionHandler, ::throwItemTo) }
 
   private fun throwItemTo(item: Item, monkeyId: MonkeyId) = monkeys[monkeyId].receive(item)
 
-  private fun addMonkey(monkey: Monkey) = monkeys.add(monkey)
-
   companion object {
     fun parse(gameConfig: String): Game {
-      val modulus = Regex("divisible by (\\d+)")
-        .findAll(gameConfig)
-        .map { it.groupValues[1].toLong() }
-        .reduce(Long::times)
-      val game = Game(modulus)
-      gameConfig.split("\n\n").forEach {
-        val monkey = Monkey.parse(it, game::throwItemTo)
-        game.addMonkey(monkey)
-      }
-      return game
+      val monkeys = gameConfig.split("\n\n").map { Monkey.parse(it) }
+      val modulus = monkeys.map { it.test.modulus }.distinct().reduce(Long::times)
+
+      return Game(modulus, monkeys)
     }
   }
 }
@@ -52,39 +33,43 @@ private data class Monkey(
   val items: MutableList<Item>,
   val operation: Operation,
   val test: Test,
-  val throwItemToFn: (Item, MonkeyId) -> Unit,
 ) {
-  fun processItems(onInspectionHandler: (MonkeyId, Item) -> Item) {
-    items.forEach { item -> processItem(item, onInspectionHandler) }
+  fun clone() = Monkey(id, items.toMutableList(), operation, test)
+
+  fun processItems(
+    onInspectionHandler: (MonkeyId, Item) -> Item,
+    throwItemToFn: (Item, MonkeyId) -> Unit,
+  ) {
+    items.forEach { item -> processItem(item, onInspectionHandler, throwItemToFn) }
     items.clear()
   }
 
-  fun clone(throwItemToFn: (Item, MonkeyId) -> Unit): Monkey {
-    return Monkey(id, items.toMutableList(), operation, test, throwItemToFn)
-  }
-
-  private fun processItem(item: Item, onInspectionHandler: ((MonkeyId, Item) -> Item)) {
+  private fun processItem(
+    item: Item,
+    onInspectionHandler: (MonkeyId, Item) -> Item,
+    throwItemToFn: (Item, MonkeyId) -> Unit,
+  ) {
     val inspectedItem = inspect(item)
     val itemToTest = onInspectionHandler(id, inspectedItem)
     val monkeyId = test.test(itemToTest)
     throwItemToFn(itemToTest, monkeyId)
   }
 
-  private fun inspect(item: Item) = Item(operation.operate(item.worryLevel))
+  private fun inspect(item: Item) = item.operate(operation)
 
   fun receive(item: Item) {
     items += item
   }
 
   companion object {
-    fun parse(monkeyConfig: String, throwItemToFn: (Item, MonkeyId) -> Unit): Monkey {
+    fun parse(monkeyConfig: String): Monkey {
       val lines = monkeyConfig.lines()
       val id = Regex("\\d+").find(lines[0])!!.value.toInt()
       val items = Item.parseMultipleItems(lines[1]).toMutableList()
       val operation = Operation.parse(lines[2])
       val test = Test.parse(lines.takeLast(3).joinToString())
 
-      return Monkey(id, items, operation, test, throwItemToFn)
+      return Monkey(id, items, operation, test)
     }
   }
 }
@@ -92,6 +77,8 @@ private data class Monkey(
 private typealias MonkeyId = Int
 
 private data class Item(val worryLevel: Long) {
+  fun operate(operation: Operation) = Item(operation.operate(worryLevel))
+
   companion object {
     fun parseSingleItem(str: String): Item = Item(str.toLong())
 
@@ -158,11 +145,10 @@ private val solution = object : Solution<Input, Output>(2022, "Day11") {
   override fun part1(input: Input): Output {
     val game = input.clone()
     val inspectionCount = mutableMapOf<MonkeyId, Long>()
-    game.onInspection { monkeyId, item ->
+    game.runMultipleRounds(20) { monkeyId, item ->
       inspectionCount[monkeyId] = inspectionCount[monkeyId]?.let { it + 1 } ?: 1
       Item(item.worryLevel / 3L)
     }
-    game.runMultipleRounds(20)
     val activeMonkeys = inspectionCount.values.sortedDescending().take(2)
     return activeMonkeys.reduce(Long::times)
   }
@@ -170,11 +156,10 @@ private val solution = object : Solution<Input, Output>(2022, "Day11") {
   override fun part2(input: Input): Output {
     val game = input.clone()
     val inspectionCount = mutableMapOf<MonkeyId, Long>()
-    game.onInspection { monkeyId, item ->
+    game.runMultipleRounds(10000) { monkeyId, item ->
       inspectionCount[monkeyId] = inspectionCount[monkeyId]?.let { it + 1 } ?: 1
       Item(item.worryLevel % game.modulus)
     }
-    game.runMultipleRounds(10000)
     val activeMonkeys = inspectionCount.values.sortedDescending().take(2)
     return activeMonkeys.reduce(Long::times)
   }
