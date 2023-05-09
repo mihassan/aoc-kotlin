d7cc85c683237f2900cb38ad3ea6ebfd99e277ff
Md Imrul Hassan <mihassan@gmail.com>
2023-05-09 16:00:26 +0700

    Solve 2022, Day 19 - Refactor Bag class to be Mutable which reduces running time from 15min to 4min.

--- src/lib/Bag.kt
+++ src/lib/Bag.kt
@@ -2,60 +2,97 @@ package lib
 
 import lib.Collections.histogram
 
-data class Bag<T> private constructor(
-  val entries: Map<T, Int> = mapOf<T, Int>().withDefault { 0 },
-) {
+@JvmInline
+@Suppress("NOTHING_TO_INLINE")
+value class Bag<T>(val entries: MutableMap<T, Int> = mutableMapOf()) {
   init {
     require(entries.all { (_, c) -> c > 0 })
   }
 
-  val size: Int get() = entries.values.sum()
+  inline val size: Int get() = entries.values.sum()
 
-  operator fun get(key: T): Int = entries.getValue(key)
+  operator fun get(key: T): Int = entries[key] ?: 0
 
-  operator fun contains(key: T): Boolean = this[key] > 0
-
-  infix fun isSubSetOf(other: Bag<T>): Boolean = entries.all { (t, c) -> c <= other[t] }
-
-  operator fun plus(key: T): Bag<T> {
-    val newEntries = entries.toMutableMap().withDefault { 0 }
-    newEntries[key] = newEntries.getValue(key) + 1
-    return Bag(newEntries)
+  operator fun set(key: T, count: Int) {
+    entries[key] = count
   }
 
-  operator fun plus(other: Bag<T>): Bag<T> {
-    val newEntries = entries.toMutableMap().withDefault { 0 }
+  inline operator fun contains(key: T): Boolean = this[key] > 0
+
+  inline infix fun isSubSetOf(other: Bag<T>): Boolean =
+    entries.all { (t, c) -> c <= other[t] }
+
+  inline operator fun plus(key: T): Bag<T> {
+    val newBag = Bag<T>()
+    newBag += this
+    newBag += key
+    return newBag
+  }
+
+  inline operator fun plus(other: Bag<T>): Bag<T> {
+    val newBag = Bag<T>()
+    newBag += this
+    newBag += other
+    return newBag
+  }
+
+  inline operator fun minus(other: Bag<T>): Bag<T> {
+    val newBag = Bag<T>()
+    newBag += this
+    newBag -= other
+    return newBag
+  }
+
+  inline operator fun minus(key: T): Bag<T> {
+    val newBag = Bag<T>()
+    newBag += this
+    newBag -= key
+    return newBag
+  }
+
+  operator fun times(multiplicand: Int): Bag<T> {
+    val newBag = Bag(entries.toMutableMap())
+    newBag *= multiplicand
+    return newBag
+  }
+
+  inline operator fun plusAssign(key: T) {
+    this[key] += 1
+  }
+
+  inline operator fun plusAssign(other: Bag<T>) {
     other.entries.forEach { (t, c) ->
-      newEntries[t] = newEntries.getValue(t) + c
+      require(c > 0)
+      this[t] += c
     }
-    return Bag(newEntries)
   }
 
-  operator fun minus(key: T): Bag<T> {
-    val newEntries = entries.toMutableMap().withDefault { 0 }
-    newEntries[key] = newEntries.getValue(key) - 1
-    if (newEntries.getValue(key) <= 0) {
-      newEntries.remove(key)
+  inline operator fun minusAssign(key: T) {
+    val newValue = this[key] - 1
+    if (newValue > 0) {
+      this[key] = newValue
+    } else {
+      entries.remove(key)
     }
-    return Bag(newEntries)
   }
 
-  operator fun minus(other: Bag<T>): Bag<T> {
-    val newEntries = entries.toMutableMap().withDefault { 0 }
+  inline operator fun minusAssign(other: Bag<T>) {
     other.entries.forEach { (t, c) ->
-      newEntries[t] = newEntries.getValue(t) - c
-      if (newEntries.getValue(t) <= 0) {
-        newEntries.remove(t)
+      val newValue = this[t] - c
+      if (newValue > 0) {
+        this[t] = newValue
+      } else {
+        entries.remove(t)
       }
     }
-    return Bag(newEntries)
   }
 
-  operator fun times(multiplicand: Int): Bag<T> =
-    Bag(entries.mapValues { (_, c) -> c * multiplicand })
+  operator fun timesAssign(multiplicand: Int) {
+    entries.replaceAll { _, c -> c * multiplicand }
+  }
 
   companion object {
-    fun <T> of(entries: Map<T, Int>): Bag<T> = Bag(entries.withDefault { 0 })
+    fun <T> of(entries: Map<T, Int>): Bag<T> = Bag(entries.toMutableMap())
     fun <T> of(items: List<T>): Bag<T> = of(items.histogram())
     fun <T> of(vararg items: T): Bag<T> = of(items.toList())
   }
