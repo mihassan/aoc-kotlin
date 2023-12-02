6edfde3915d143d9017c325803e950582b07e64e
Md Imrul Hassan <mihassan@gmail.com>
2023-12-02 13:33:27 +1100

    Solve 2023 Day01 - Reformatting code.

--- src/aoc23/Day01.kt
+++ src/aoc23/Day01.kt
@@ -9,32 +9,29 @@ typealias Input = List<String>
 typealias Output = Int
 
 private val solution = object : Solution<Input, Output>(2023, "Day01") {
-  override fun parse(input: String): Input {
-    return input.lines()
-  }
+  override fun parse(input: String): Input = input.lines()
 
-  override fun format(output: Output): String {
-    return "$output"
-  }
+  override fun format(output: Output): String = "$output"
 
-  override fun part1(input: Input): Output {
-    return input.sumOf { line ->
-      line.filter { it.isDigit() }.run { "${first()}${last()}".toInt() }
+  override fun part1(input: Input): Output = input.sumOf { line ->
+    line.filter {
+      it.isDigit()
+    }.run {
+      "${first()}${last()}".toInt()
     }
   }
 
-  override fun part2(input: Input): Output {
-    return input.sumOf { line ->
-      val firstDigit = numbers.find(line)!!.value.let { numberToDigit[it] ?: it.toInt() }
-      val lastDigit = numbers.findLast(line)!!.let { numberToDigit[it] ?: it.toInt() }
-      10 * firstDigit + lastDigit
-    }
+  override fun part2(input: Input): Output = input.sumOf { line ->
+    val firstDigit = numbers.find(line)!!.value.let { numberToDigit[it] ?: it.toInt() }
+    val lastDigit = numbers.findLast(line)!!.let { numberToDigit[it] ?: it.toInt() }
+    10 * firstDigit + lastDigit
   }
 
   private fun Regex.findLast(input: String): String? =
     this.pattern.reversed().toRegex().find(input.reversed())?.value?.reversed()
 
-  private val numbers = """1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine""".toRegex()
+  private val numbers =
+    """1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine""".toRegex()
   private val numberToDigit = mapOf(
     "one" to 1,
     "two" to 2,
