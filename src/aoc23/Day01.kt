aac3b8e025ff47a35fd379ab24262a81f404a1fb
Md Imrul Hassan <mihassan@gmail.com>
2023-12-02 14:17:49 +1100

    Solve 2023 Day01 - Reformat to make structure of both parts similar. Also, consider '0' digit which I missed earlier.

--- src/aoc23/Day01.kt
+++ src/aoc23/Day01.kt
@@ -14,25 +14,29 @@ private val solution = object : Solution<Input, Output>(2023, "Day01") {
   override fun format(output: Output): String = "$output"
 
   override fun part1(input: Input): Output = input.sumOf { line ->
-    line.filter {
-      it.isDigit()
-    }.run {
-      "${first()}${last()}".toInt()
-    }
+    val firstDigit = line.firstOrNull { it.isDigit() } ?: error("No first digit")
+    val lastDigit = line.lastOrNull { it.isDigit() } ?: error("No last digit")
+    "${firstDigit}${lastDigit}".toInt()
   }
 
   override fun part2(input: Input): Output = input.sumOf { line ->
-    val firstDigit = numbers.find(line)!!.value.let { numberToDigit[it] ?: it.toInt() }
-    val lastDigit = numbers.findLast(line)!!.let { numberToDigit[it] ?: it.toInt() }
-    10 * firstDigit + lastDigit
+    val firstDigit = numbers.findFirst(line)?.wordToInt() ?: error("No first digit")
+    val lastDigit = numbers.findLast(line)?.wordToInt() ?: error("No last digit")
+    "${firstDigit}${lastDigit}".toInt()
   }
+
+  private fun Regex.findFirst(input: String): String? =
+    this.find(input)?.value
 
   private fun Regex.findLast(input: String): String? =
     this.pattern.reversed().toRegex().find(input.reversed())?.value?.reversed()
 
+  private fun String.wordToInt(): Int? = wordToIntMap[this] ?: toIntOrNull()
+
   private val numbers =
-    """1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine""".toRegex()
-  private val numberToDigit = mapOf(
+    """0|1|2|3|4|5|6|7|8|9|zero|one|two|three|four|five|six|seven|eight|nine""".toRegex()
+  private val wordToIntMap = mapOf(
+    "zero" to 0,
     "one" to 1,
     "two" to 2,
     "three" to 3,
