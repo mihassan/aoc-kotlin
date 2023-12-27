6d119a7b71b618b75c9f9556793fbbe4088abdde
Md Imrul Hassan <mihassan@gmail.com>
2023-12-27 15:40:20 +1100

    Solve 2023 Day 11 - Part 2 - Implement using a different approach that covers both parts.

--- src/aoc23/Day11.kt
+++ src/aoc23/Day11.kt
@@ -3,49 +3,68 @@
 package aoc23.day11
 
 import kotlin.math.abs
+import lib.Collections.cumulativeSum1
 import lib.Grid
 import lib.Point
 import lib.Solution
 
-typealias Input = Grid<Char>
+data class Image(val galaxies: List<Point>, val maxBound: Point) {
+  fun expandRows(count: Int): Image {
+    val rowSizes = (0..maxBound.y).map { if (isEmptyRow(it)) count else 1 }
+    val lastRow = rowSizes.last()
+    val newRows = rowSizes.cumulativeSum1().dropLast(1)
+    val newGalaxies = galaxies.map { it.copy(y = newRows[it.y]) }
+    return Image(newGalaxies, maxBound.copy(y = lastRow))
+  }
 
-typealias Output = Int
+  fun expandCols(count: Int): Image = transpose().expandRows(count).transpose()
+
+  private fun isEmptyRow(row: Int) = galaxies.none { galaxy -> galaxy.y == row }
+
+  private fun transpose() = Image(galaxies.map { it.transpose() }, maxBound.transpose())
+
+  private fun Point.transpose() = Point(y, x)
+
+  companion object {
+    fun parse(input: String): Image {
+      val grid = Grid.parse(input)
+      val galaxies = grid.indicesOf('#')
+      val maxBound = Point(galaxies.maxOf { it.x }, galaxies.maxOf { it.y })
+      return Image(galaxies, maxBound)
+    }
+  }
+}
+
+typealias Input = Image
+
+typealias Output = Long
 
 private val solution = object : Solution<Input, Output>(2023, "Day11") {
-  override fun parse(input: String): Input = Grid.parse(input)
+  override fun parse(input: String): Input = Image.parse(input)
 
   override fun format(output: Output): String = "$output"
 
-  override fun part1(input: Input): Output {
-    val expandedImage: Input = input.expandRows().expandCols()
-    val galaxies = expandedImage.indicesOf('#')
-    return galaxies.sumOf { g1 ->
-      galaxies.sumOf { g2 ->
-        if (g1 < g2) distance(g1, g2) else 0
+  override fun solve(part: Part, input: Input): Output =
+    with(expand(part, input)) {
+      galaxies.sumOf { g1 ->
+        galaxies.sumOf { g2 ->
+          if (g1 < g2) g1.manhattanDistance(g2).toLong() else 0L
+        }
       }
     }
-  }
 
-  override fun part2(input: Input): Output {
-    TODO("Not yet implemented")
-  }
-
-  private fun Input.expandRows(): Input =
-    Grid(grid.flatMap { row ->
-      if (row.all { it == '.' }) {
-        listOf(row, row)
-      } else {
-        listOf(row)
-      }
-    })
-
-  private fun Input.expandCols(): Input =
-    transposed().expandRows().transposed()
-
-  private fun distance(g1: Point, g2: Point): Int =
-    (g1 - g2).let { (dx, dy) ->
-      abs(dx) + abs(dy)
+  private fun expand(part: Part, input: Input): Input =
+    EXPANSION_FACTOR[part]!!.let { factor ->
+      input.expandRows(factor).expandCols(factor)
     }
+
+  private fun distance(p1: Point, p2: Point): Long =
+    (abs(p1.x - p2.x) + abs(p1.y - p2.y)).toLong()
+
+  private val EXPANSION_FACTOR = mapOf(
+    Part.PART1 to 2,
+    Part.PART2 to 1000000
+  )
 }
 
 fun main() = solution.run()
