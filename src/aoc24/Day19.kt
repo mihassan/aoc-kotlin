363d6cedb9fefeb48ee7088b6fd1f28a9c6c15f5
Md Imrul Hassan <mihassan@gmail.com>
2025-02-27 21:51:22 +1100

    Refactor 2024 Day 19 problem by merging solutions for both parts and utilizing some Map methods.

--- src/aoc24/Day19.kt
+++ src/aoc24/Day19.kt
@@ -21,48 +21,22 @@ private val solution = object : Solution<Input, Output>(2024, "Day19") {
   override fun format(output: Output): String = "$output"
 
   override fun part1(input: Input): Output =
-    input.designs.count { it.canBeMadeWith(input.patterns) }.toLong()
+    input.designs.count { it.makeDesign(input.patterns) > 0 }.toLong()
 
-  override fun part2(input: Input): Output =
-    input.designs.sumOf { it.canBeMadeHowManyWaysWith(input.patterns) }
+  override fun part2(input: Input): Output = input.designs.sumOf { it.makeDesign(input.patterns) }
 
-  private fun String.canBeMadeWith(patterns: List<String>): Boolean {
-    val memo = mutableMapOf<String, Boolean>()
-
-    fun String.recursivelyCanBeMadeWith(): Boolean {
-      if (isEmpty()) return true
-      if (this in memo) return memo[this]!!
-
-      val result = patterns.any { pattern ->
-        endsWith(pattern) && dropLast(pattern.length).recursivelyCanBeMadeWith()
-      }
-
-      memo[this] = result
-      return result
-    }
-
-    return recursivelyCanBeMadeWith()
-  }
-
-  private fun String.canBeMadeHowManyWaysWith(patterns: List<String>): Long {
-    val memo = mutableMapOf<String, Long>()
-
-    fun String.recursivelyCanBeMadeHowManyWaysWith(): Long {
-      if (isEmpty()) return 1
-      if (this in memo) return memo[this]!!
-
-      val result = patterns.sumOf { pattern ->
-        if (endsWith(pattern)) dropLast(pattern.length).recursivelyCanBeMadeHowManyWaysWith()
+  private fun String.makeDesign(
+    patterns: List<String>,
+    cache: MutableMap<String, Long> = mutableMapOf<String, Long>(),
+  ): Long {
+    if (isEmpty()) return 1
+    return cache.getOrPut(this) {
+      patterns.sumOf { pattern ->
+        if (this.startsWith(pattern)) removePrefix(pattern).makeDesign(patterns, cache)
         else 0
       }
-
-      memo[this] = result
-      return result
     }
-
-    return recursivelyCanBeMadeHowManyWaysWith()
   }
 }
-
 
 fun main() = solution.run()
