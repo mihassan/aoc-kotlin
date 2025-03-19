9351f41c3500db89158bb2aa6d2020be522983b7
Md Imrul Hassan <mihassan@gmail.com>
2025-03-20 00:38:49 +1100

    Refactor 2024 Day 23 solution by going with imperative style coding. This seems significantly faster (100x) for part 2. The main efficiency gain was by avoiding repeated checking of same set of nodes.

--- src/aoc24/Day23.kt
+++ src/aoc24/Day23.kt
@@ -2,78 +2,32 @@
 
 package aoc24.day23
 
-import aoc24.day23.Clique.Companion.cliqueOf
-import kotlin.collections.sorted
 import lib.Solution
 
-@JvmInline
-value class Node(val node: String) {
-  fun isChiefNode() = node.first() == 't'
-}
-
-@JvmInline
-value class Edge(val edge: Pair<Node, Node>) {
-  val reversed: Edge get() = Edge(edge.second to edge.first)
-
-  companion object {
-    fun parse(edgeString: String): Edge {
-      val (a, b) = edgeString.split("-")
-      return Edge(Node(a) to Node(b))
-    }
-  }
-}
-
-@JvmInline
-value class Clique(val nodes: Set<Node>) {
-  fun add(node: Node): Clique = Clique(nodes + node)
-
-  companion object {
-    fun cliqueOf(node: Node): Clique = Clique(setOf(node))
-  }
+data class Node(val node: String) : Comparable<Node> {
+  fun isChiefNode(): Boolean = node.startsWith("t")
+  override fun compareTo(other: Node): Int = node.compareTo(other.node)
 }
 
 data class Graph(val nodes: Set<Node>, val edges: Map<Node, Set<Node>>) {
-  fun findThreeNodes(): Set<Set<Node>> = nodes.flatMap { firstNode ->
-    edges[firstNode]?.flatMap { secondNode ->
-      edges[secondNode]?.mapNotNull { thirdNode ->
-        setOf(firstNode, secondNode, thirdNode).takeIf { firstNode in edges[thirdNode]!! }
-      } ?: emptySet()
-    } ?: emptySet()
-  }.toSet()
-
-  fun findAllCliques(): Set<Clique>  {
-    val visited = mutableSetOf<Clique>()
-    val queue = ArrayDeque(nodes.map { cliqueOf(it) })
-
-    while (queue.isNotEmpty()) {
-      val clique = queue.removeFirst()
-      nodes.forEach { node ->
-        val newClique = addToClique(clique, node)
-        if (newClique != null && newClique !in visited) {
-          queue.add(newClique)
-          visited.add(newClique)
-        }
-      }
-    }
-
-    return visited
-  }
-
-  private fun addToClique(clique: Clique, node: Node): Clique? {
-    if (node in clique.nodes) return null
-    val reachableNodes = edges[node]?.toSet() ?: return null
-    if ((clique.nodes - reachableNodes).isNotEmpty()) return null
-    return Clique(clique.nodes + node)
-  }
-
   companion object {
-    fun fromEdges(edges: List<Edge>): Graph {
-      val nodes = edges.flatMap { listOf(it.edge.first, it.edge.second) }.toSet()
-      val doubledEdges = edges.flatMap { listOf(it, it.reversed) }
-      val edgesByNode = doubledEdges
-        .groupBy({ it.edge.first }, { it.edge.second })
-        .mapValues { it.value.toSet() }
-      return Graph(nodes, edgesByNode)
+    fun parse(graphStr: String): Graph {
+      val nodes = mutableSetOf<Node>()
+      val edges = mutableMapOf<Node, MutableSet<Node>>()
+
+      graphStr.lines().forEach { line ->
+        val (a, b) = line.split("-")
+        val nodeA = Node(a)
+        val nodeB = Node(b)
+
+        nodes += nodeA
+        nodes += nodeB
+
+        edges.getOrPut(nodeA) { mutableSetOf() } += nodeB
+        edges.getOrPut(nodeB) { mutableSetOf() } += nodeA
+      }
+
+      return Graph(nodes, edges)
     }
   }
 }
@@ -83,16 +37,72 @@ typealias Input = Graph
 typealias Output = String
 
 private val solution = object : Solution<Input, Output>(2024, "Day23") {
-  override fun parse(input: String): Input = input.lines().map(Edge::parse).let(Graph::fromEdges)
+  override fun parse(input: String): Input = Graph.parse(input)
 
   override fun format(output: Output): String = output
 
-  override fun part1(input: Input): Output =
-    input.findThreeNodes().filter { it.any { it.isChiefNode() } }.let { return "${it.size}" }
+  override fun part1(input: Input): Output {
+    var result = 0
+
+    input.nodes.forEach { node1 ->
+
+      input.edges[node1]!!.forEach { node2 ->
+        // Avoid duplicates by only considering nodes in ascending order
+        if (node1 > node2) return@forEach
+
+        input.edges[node2]!!.forEach { node3 ->
+          // Avoid duplicates by only considering nodes in ascending order
+          if (node2 > node3) return@forEach
+
+          // If the edge is missing, the nodes are not connected
+          if (node3 !in input.edges[node1]!!) return@forEach
+
+          // If any of the nodes is a chief node, increment the result
+          if (listOf(node1, node2, node3).any { it.isChiefNode() }) {
+            result++
+          }
+        }
+      }
+    }
+
+    return "$result"
+  }
 
   override fun part2(input: Input): Output {
-    val maxClique = input.findAllCliques().maxBy { it.nodes.size }
-    return maxClique.nodes.map { it.node }.sorted().joinToString(",")
+    val clique = ArrayDeque<Node>()
+    var maxClique = emptyList<Node>()
+
+    fun dfs(node: Node) {
+      if (clique.size > maxClique.size) {
+        maxClique = clique.toList()
+      }
+
+      input.edges[node]?.forEach { newNode ->
+        // Skip if the node is already in the clique
+        if (newNode in clique) return@forEach
+
+        // Avoid checking the same clique multiple times by only considering nodes in ascending order
+        if (clique.any { it > newNode }) return@forEach
+
+        // Skip if the new node is not connected to all nodes in the clique
+        val newEdges = input.edges[newNode]!!
+        if (clique.any { it !in newEdges }) return@forEach
+
+        // Add the new node to the clique and continue the search
+        clique.addLast(newNode)
+        dfs(newNode)
+        clique.removeLast()
+      }
+    }
+
+    // Start the search from each node in the graph
+    input.nodes.forEach {
+      clique.add(it)
+      dfs(it)
+      clique.clear()
+    }
+
+    return maxClique.joinToString(",") { it.node }
   }
 }
 
