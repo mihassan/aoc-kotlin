5850473c69b17d6b60b200e78febd46292f88499
Md Imrul Hassan <mihassan@gmail.com>
2025-12-16 12:32:48 +0700

    Solve 2025 Day 2 problem.

--- src/aoc25/Day02.kt
+++ src/aoc25/Day02.kt
@@ -4,22 +4,53 @@ package aoc25.day02
 
 import lib.Solution
 
-typealias Input = List<String>
+data class ProductIdRange(val start: Long, val end: Long) {
+  val range: LongRange
+    get() = start..end
 
-typealias Output = Int
+  val digits: Int
+    get() = "$end".length
+
+  fun sumOfBasicInvalidIds(): Long =
+    range.filter { it.hasRepeatingPattern(2) }.sum()
+
+  fun sumOfExtendedInvalidIds(): Long =
+    range.filter { n ->
+      (2..digits).any { r ->
+        n.hasRepeatingPattern(r)
+      }
+    }.sum()
+
+  fun Long.hasRepeatingPattern(repetition: Int): Boolean {
+    val numStr = "$this"
+    val strLen = numStr.length
+    if (strLen % repetition != 0) return false
+    val patternLen = strLen / repetition
+    if (patternLen == 0) return false
+    return numStr.take(patternLen).repeat(repetition) == numStr
+  }
+
+  companion object {
+    fun parse(rangeStr: String): ProductIdRange {
+      val (start, end) = rangeStr.split("-").map { it.toLong() }
+      return ProductIdRange(start, end)
+    }
+  }
+}
+
+typealias Input = List<ProductIdRange>
+
+typealias Output = Long
 
 private val solution = object : Solution<Input, Output>(2025, "Day02") {
-  override fun parse(input: String): Input = input.lines()
+  override fun parse(input: String): Input = input.split(",").map(ProductIdRange.Companion::parse)
 
   override fun format(output: Output): String = "$output"
 
-  override fun part1(input: Input): Output {
-    TODO("Implement part 1")
-  }
+  override fun part1(input: Input): Output = input.sumOf { it.sumOfBasicInvalidIds() }
 
-  override fun part2(input: Input): Output {
-    TODO("Implement part 2")
-  }
+  override fun part2(input: Input): Output =
+    input.sumOf { it.sumOfExtendedInvalidIds() }
 }
 
 fun main() = solution.run()
