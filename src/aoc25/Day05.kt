f1380b66061ad7f9889d5ab9ec0ee83b431828e6
Md Imrul Hassan <mihassan@gmail.com>
2025-12-18 14:58:01 +0700

    Solve 2025 Day 5 problem.

--- src/aoc25/Day05.kt
+++ src/aoc25/Day05.kt
@@ -4,21 +4,52 @@ package aoc25.day05
 
 import lib.Solution
 
-typealias Input = List<String>
+data class Input(
+  val freshIngredients: List<LongRange>,
+  val ingredients: List<Long>,
+) {
+  companion object {
+    const val SECTION_DELIMITER = "\n\n"
 
-typealias Output = Int
+    fun parse(inputStr: String): Input {
+      val (freshIngredientsStr, ingredientsStr) = inputStr.split(SECTION_DELIMITER)
+      val freshIngredients = freshIngredientsStr.lines().map { line ->
+        val (start, end) = line.split("-").map { it.toLong() }
+        start..end
+      }
+      val ingredients = ingredientsStr.lines().map { it.toLong() }
+      return Input(freshIngredients, ingredients)
+    }
+  }
+}
+
+typealias Output = Long
 
 private val solution = object : Solution<Input, Output>(2025, "Day05") {
-  override fun parse(input: String): Input = input.lines()
+  override fun parse(input: String): Input = Input.parse(input)
 
   override fun format(output: Output): String = "$output"
 
-  override fun part1(input: Input): Output {
-    TODO("Implement part 1")
-  }
+  override fun part1(input: Input): Output = input.ingredients.count { ingredient ->
+    input.freshIngredients.any { range -> ingredient in range }
+  }.toLong()
 
-  override fun part2(input: Input): Output {
-    TODO("Implement part 2")
+  override fun part2(input: Input): Output =
+    input.freshIngredients.mergeOverlapping().sumOf { it.last - it.first + 1 }
+
+  /** Merges overlapping or adjacent ranges into consolidated ranges. */
+  private fun List<LongRange>.mergeOverlapping(): List<LongRange> {
+    val sortedRanges = sortedBy { it.first }
+    val mergedRanges = mutableListOf<LongRange>()
+    for (range in sortedRanges) {
+      val last = mergedRanges.lastOrNull()
+      if (last != null && range.first <= last.last + 1) {
+        mergedRanges[mergedRanges.lastIndex] = last.first..maxOf(last.last, range.last)
+      } else {
+        mergedRanges += range
+      }
+    }
+    return mergedRanges
   }
 }
 
